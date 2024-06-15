fcd96b2145b4e63edb743c04a989ccde50bdcbe6
daviddorf <ddorf@tritonsys.com>
2024-06-15 12:06:42 -0400

    Update README, add script for sourcing env variables and install. Add separate WAM-V model with ardupilot for optional compatibility.

---
+++ setup.sh
@@ -0,0 +1,11 @@
+export GZ_SIM_RESOURCE_PATH=\
+$GZ_SIM_RESOURCE_PATH:\
+./src/asv_wave_sim/gz-waves-models/models:\
+./src/asv_wave_sim/gz-waves-models/world_models:\
+./src/asv_wave_sim/gz-waves-models/worlds
+
+export GZ_SIM_SYSTEM_PLUGIN_PATH=\
+$GZ_SIM_SYSTEM_PLUGIN_PATH:\
+./install/lib
+
+source install/setup.sh
