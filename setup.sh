2b38854e56615167402c1b7934d40eac1f43cb5d
daviddorf <ddorf@tritonsys.com>
2024-06-15 12:16:05 -0400

    Move waves to models. Delete world models folder. Make waves more intense. Move sun up vertically.

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
 export GZ_SIM_RESOURCE_PATH=\
 $GZ_SIM_RESOURCE_PATH:\
 ./src/asv_wave_sim/gz-waves-models/models:\
-./src/asv_wave_sim/gz-waves-models/world_models:\
 ./src/asv_wave_sim/gz-waves-models/worlds
 
 export GZ_SIM_SYSTEM_PLUGIN_PATH=\
