6792d9aa87b44cb2d981840e71023bf132762656
daviddorf <ddorf@tritonsys.com>
2024-06-17 10:15:30 -0400

    Renaming and organizing into worlds, models, and plugins folders.

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 export GZ_SIM_RESOURCE_PATH=\
 $GZ_SIM_RESOURCE_PATH:\
-./src/asv_wave_sim/gz-waves-models/models:\
-./src/asv_wave_sim/gz-waves-models/worlds
+./src/asv_wave_sim/models:\
+./src/asv_wave_sim/worlds
 
 export GZ_SIM_SYSTEM_PLUGIN_PATH=\
 $GZ_SIM_SYSTEM_PLUGIN_PATH:\
